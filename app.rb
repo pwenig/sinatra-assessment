fae7826e01fbaccf13021d7ff47fa9eaf54bd1f9
Paul Wenig <pwenig@gmail.com>
2014-03-31 09:26:37 -0600

    user sees welcome on index with link to add product

--- app.rb
+++ app.rb
@@ -4,4 +4,7 @@ class App < Sinatra::Application
   get '/' do
     erb :index
   end
+  get '/add' do
+    erb :add
+  end
 end
--- spec/tasker_spec.rb
+++ spec/tasker_spec.rb
@@ -2,3 +2,12 @@ require 'spec_helper'
 require 'capybara/rspec'
 require_relative '../app'
 Capybara.app = App
+
+feature 'managing product list' do
+  scenario 'adding tasks to list' do
+    visit('/')
+    expect(page).to have_content "Welcome"
+    click_on ('Add a Product')
+
+  end
+end
